19dca5a2054ea269daa436405d41f7987e5b3e56
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2022-04-01 01:17:42 -0300

    [ADDED] Patrolling State
    Enemy now patrols between points A, B and C.

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -4,14 +4,66 @@ using UnityEngine;
 using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
+    [Header("Agent")]
     public NavMeshAgent agent;
-    public GameObject player;
-    // Start is called before the first frame update
 
+    [Header("PlayerObjects")]
+    public GameObject player;
+
+    [Header("PatrolPointObjects")]
+    public GameObject pointDest;
+    public GameObject pointA;
+    public GameObject pointB;
+    public GameObject pointC;
+
+    [Header("Scripts")]
+    public StateMachine stateMachine;
+
+    /*[HideInInspector]*/
+    public int state;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        stateMachine.GetComponent<StateMachine>();
+        pointDest.transform.position = pointA.transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.transform.position);
+       switch (stateMachine.currentState)
+       {
+            case 1:
+                // patrol 3 points on map
+                agent.SetDestination(pointDest.transform.position);                
+                break;
+            case 2:
+                // lost sight of player
+                agent.SetDestination(agent.transform.position);
+                break;
+            case 3:
+                // chases player
+                agent.SetDestination(player.transform.position);
+                break;
+            case 4:
+                // attacks the player
+                agent.SetDestination(player.transform.position);
+                break;
+            case 5:
+                // goes back to partol
+                agent.SetDestination(pointDest.transform.position);
+                break;
+       }
+
+        // Swap dest to patrol points
+        if (agent.transform.position.x == pointA.transform.position.x
+            && pointDest.transform.position == pointA.transform.position) pointDest.transform.position = pointB.transform.position;
+
+        else if (agent.transform.position.x == pointB.transform.position.x
+            && pointDest.transform.position == pointB.transform.position) pointDest.transform.position = pointC.transform.position;
+
+        else if (agent.transform.position.x == pointC.transform.position.x
+            && pointDest.transform.position == pointC.transform.position) pointDest.transform.position = pointA.transform.position;
     }
 }
--- Assets/StateMachine.cs
+++ Assets/StateMachine.cs
@@ -13,6 +13,9 @@ public class StateMachine : MonoBehaviour
         retreating
     }
 
+    [HideInInspector]
+    public int currentState;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,18 +31,23 @@ public class StateMachine : MonoBehaviour
         {
             case EnemyState.patroling:
                 // is patroling
+                currentState = 1;
                 break;
             case EnemyState.searching:
                 // is searching
+                currentState = 2;
                 break;
             case EnemyState.chaseing:
                 // is chasing
+                currentState = 3;
                 break;
             case EnemyState.attacking:
                 // is attacking
+                currentState = 4;
                 break;
             case EnemyState.retreating:
                 // is retreating
+                currentState = 5;
                 break;
         }
         /*if (enemyState == EnemyState.patrol)
